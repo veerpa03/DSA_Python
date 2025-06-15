e219fc0f0663b367e45a8d02f7c9e3feb4c0a80e
veerpa03 <virajpa003@gmail.com>
2025-06-14 23:34:09 -0700

    arrays

---
+++ Arrays/First.py
@@ -0,0 +1,21 @@
+numbers = [1,2,3,4,5,6,7,8,9,10]
+print(numbers[1])
+
+lists = [1,2,3, "viraj", "Apple", 2.22]
+print(lists[5])
+
+my_list = [5,10,15,20]
+print(my_list[-2])
+
+
+my_list2 = [[1,2,3,], "Viraj", [4,5,6]]
+
+print(my_list2[1][1])
+
+my_list3 = [[1,2,3], [['a', 'b', 'c'], 5, 6]]
+
+print(my_list3[1][0][1])
+
+
+
+
