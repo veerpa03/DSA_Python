0b87efa4e6c8faa3905742d27b949d7c82b53ec2
veerpa03 <virajpa003@gmail.com>
2025-07-09 00:00:34 -0400

    commit

--- Maths/prime.py
+++ Maths/prime.py
@@ -10,17 +10,14 @@
 
 # print(prime(70))
 
-def prime_fact(n):
-    num=n
-    for i in range (2, n):
-        if n%i==0:
-            curr=i
-            curr=min(curr,i)
-            num=num//curr
-prime_fact(100)
+# def prime_fact(n):
+#     num=n
+#     for i in range (2, n):
+#         if n%i==0:
+#             curr=i
+#             curr=min(curr,i)
+#             num=num//curr
+# prime_fact(100)
 
 
 
-1 2 3 6 4 2 0
-4 3 2 1 0
-1 2 3 4 5 7 
